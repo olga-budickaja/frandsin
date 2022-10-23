cc8a068212dd1d6723580c82de24c15ec340732b
Olga Budickaja <olgabudickaja@gmail.com>
2022-10-23 07:11:53 +0300

    add Sidebar

--- src/Components/Main/Sidebar/SideBarItem.jsx
+++ src/Components/Main/Sidebar/SideBarItem.jsx
@@ -1,16 +1,33 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import {NavLink} from "react-router-dom";
 import style from "./SideBarItem.module.scss";
+import {collection, getDocs} from "firebase/firestore";
+import {db} from "../../../firebase-config/firebase-config";
 
 const SideBarItem = (props) => {
-    let path = "fransin/profiles/" + props.name
-    return (
-            <li className={style.item}>
-                <NavLink to={path} className={style.itemLink}>
-                    <img className={style.itemImg} src={props.src} alt={props.name}/>
-                </NavLink>
-            </li>
+    const [users, setUsers] = useState([]);
+    const usersCollectionRef = collection(db, "users");
 
+    const getUsers = async () => {
+        const data = await getDocs(usersCollectionRef);
+        setUsers(data.docs.map((doc) => ({...doc.data(), id: doc.id})));
+        console.log(data);
+    }
+
+    useEffect(() => {
+        getUsers();
+    }, []);
+
+    return ( <>{users.slice(1, 6).map(user =>
+
+        <li className={style.item}>
+            <NavLink to={`frandsin/profiles/${user.name.replace(" ", "-")}`} className={style.itemLink}>
+                <img className={style.itemImg} src={user.src} alt={user.name}/>
+            </NavLink>
+        </li>
+    )
+    }
+    </>
     )
 }
 
